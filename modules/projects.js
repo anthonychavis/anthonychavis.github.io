6bc210a7c931a54b12e5c15b2924e25a283fea44
Anthony C <gitanthony@yahoo.com>
2022-09-30 16:35:35 -0400

    modules implemented - projects/handlebars

---
+++ modules/projects.js
@@ -0,0 +1,121 @@
+// HANDLEBARS
+const context = {
+    projects: [
+        // {
+        //     title: 'Chore Door',
+        //     imgSrc: 'resources/images/choredoor-mobile-landscape-resize.jpg',
+        //     imgAlt: 'Chore Door game in mobile landscape view',
+        //     siteLive: 'https://anthonychavis.github.io/chore-door/',
+        //     siteCode: 'https://github.com/anthonychavis/chore-door',
+        // },
+        // {
+        //     title:
+        //     imgSrc:
+        //     imgAlt:
+        //     siteLive:
+        //     siteCode:
+        // },
+    ],
+};
+
+// Parent Class
+class Projects {
+    constructor(title, imgSrc, imgAlt, siteLive, siteCode) {
+        this._title = title;
+        this._imgSrc = imgSrc;
+        this._imgAlt = imgAlt;
+        this._siteLive = siteLive;
+        this._siteCode = siteCode;
+
+        this.keyValue();
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get imgSrc() {
+        return this._imgSrc;
+    }
+
+    get imgAlt() {
+        return this._imgAlt;
+    }
+
+    get siteLive() {
+        return this._siteLive;
+    }
+
+    get siteCode() {
+        return this._siteCode;
+    }
+
+    keyValue() {
+        const pairs = {
+            title: `${this.title}`,
+            imgSrc: `${this.imgSrc}`,
+            imgAlt: `${this.imgAlt}`,
+            siteLive: `${this.siteLive}`,
+            siteCode: `${this.siteCode}`,
+        };
+
+        this.pushToContext(pairs);
+    }
+
+    pushToContext(newProject) {
+        context.projects.push(newProject);
+    }
+}
+
+// Chore Door
+new Projects(
+    'Chore Door',
+    'resources/images/choredoor-mobile-landscape-resize.jpg',
+    'Chore Door game in mobile landscape view',
+    'https://anthonychavis.github.io/chore-door/',
+    'https://github.com/anthonychavis/chore-door'
+);
+
+// Pig Game
+new Projects(
+    'Pig Game',
+    'resources/images/pig-game2.png',
+    'Pig Game in mobile landscape view',
+    'https://anthonychavis.github.io/pig_game/',
+    'https://github.com/anthonychavis/pig_game'
+);
+
+// Guess My Number
+new Projects(
+    'Guess My Number',
+    'resources/images/guessnumber.png',
+    'Guess My Number game in mobile landscape view',
+    'https://anthonychavis.github.io/guessmynumber/',
+    'https://github.com/anthonychavis/guessmynumber'
+);
+
+// Excursion
+new Projects(
+    'Excursion',
+    'resources/images/campground.jpg',
+    'campground with tents in the evening',
+    'https://anthonychavis.github.io/excursion/',
+    'https://github.com/anthonychavis/excursion'
+);
+
+/////////////
+// export let compiledHtml;
+const projectsRender = () => {
+    if (window.location.href.endsWith('projects.html')) {
+        // select source html
+        const templateSource = document.getElementById('projectHB').innerHTML;
+        // fxn
+        const template = Handlebars.compile(templateSource);
+        // call fxn w/ object
+        const compiledHtml = template(context);
+        // assign
+        document.getElementById('project-list').innerHTML = compiledHtml;
+    }
+};
+
+export default projectsRender;
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-'use strict';
+// for projects pg
+import projects from './modules/projects.js';
+
 /*
 const bgImg = async () =>
     (document.body.style.background =
@@ -18,7 +20,9 @@ window.addEventListener('load', async () => {
 
     // await bgImg();
 
-    await timer(200).then(() => transitionEl.classList.remove('is-active')); // set time same as desired
+    // timer(200).then(() => transitionEl.classList.remove('is-active')); // set time as desired
+    let shortDelay = await timer(200);
+    shortDelay = transitionEl.classList.remove('is-active'); // set time as desired
 
     navAnchors.forEach(anchor => {
         anchor.addEventListener('click', e => {
@@ -56,127 +60,5 @@ window.addEventListener('pageshow', event => {
     }
 });
 
-// HANDLEBARS
-if (window.location.href.endsWith('projects.html')) {
-    const context = {
-        projects: [
-            // {
-            //     title: 'Chore Door',
-            //     imgSrc: 'resources/images/choredoor-mobile-landscape-resize.jpg',
-            //     imgAlt: 'Chore Door game in mobile landscape view',
-            //     siteLive: 'https://anthonychavis.github.io/chore-door/',
-            //     siteCode: 'https://github.com/anthonychavis/chore-door',
-            // },
-            // {
-            //     title: 'Musicon',
-            //     // imgSrc:
-            //     // imgAlt:
-            //     siteLive: 'https://anthonychavis.github.io/musicon/index.html',
-            //     siteCode: 'https://github.com/anthonychavis/musicon',
-            // },
-            // {
-            //     title:
-            //     imgSrc:
-            //     imgAlt:
-            //     siteLive:
-            //     siteCode:
-            // },
-        ],
-    };
-
-    // Parent Class
-    class Projects {
-        constructor(title, imgSrc, imgAlt, siteLive, siteCode) {
-            this._title = title;
-            this._imgSrc = imgSrc;
-            this._imgAlt = imgAlt;
-            this._siteLive = siteLive;
-            this._siteCode = siteCode;
-
-            this.keyValue();
-        }
-
-        get title() {
-            return this._title;
-        }
-
-        get imgSrc() {
-            return this._imgSrc;
-        }
-
-        get imgAlt() {
-            return this._imgAlt;
-        }
-
-        get siteLive() {
-            return this._siteLive;
-        }
-
-        get siteCode() {
-            return this._siteCode;
-        }
-
-        keyValue() {
-            const pairs = {
-                title: `${this.title}`,
-                imgSrc: `${this.imgSrc}`,
-                imgAlt: `${this.imgAlt}`,
-                siteLive: `${this.siteLive}`,
-                siteCode: `${this.siteCode}`,
-            };
-
-            this.pushToContext(pairs);
-        }
-
-        pushToContext(newProject) {
-            context.projects.push(newProject);
-        }
-    }
-
-    // Chore Door
-    new Projects(
-        'Chore Door',
-        'resources/images/choredoor-mobile-landscape-resize.jpg',
-        'Chore Door game in mobile landscape view',
-        'https://anthonychavis.github.io/chore-door/',
-        'https://github.com/anthonychavis/chore-door'
-    );
-
-    // Pig Game
-    new Projects(
-        'Pig Game',
-        'resources/images/pig-game2.png',
-        'Pig Game in mobile landscape view',
-        'https://anthonychavis.github.io/pig_game/',
-        'https://github.com/anthonychavis/pig_game'
-    );
-
-    // Guess My Number
-    new Projects(
-        'Guess My Number',
-        'resources/images/guessnumber.png',
-        'Guess My Number game in mobile landscape view',
-        'https://anthonychavis.github.io/guessmynumber/',
-        'https://github.com/anthonychavis/guessmynumber'
-    );
-
-    // Excursion
-    new Projects(
-        'Excursion',
-        'resources/images/campground.jpg',
-        'campground with tents in the evening',
-        'https://anthonychavis.github.io/excursion/',
-        'https://github.com/anthonychavis/excursion'
-    );
-
-    /////////////
-
-    // select source html
-    const templateSource = document.getElementById('projectHB').innerHTML;
-    // fxn
-    const template = Handlebars.compile(templateSource);
-    // call fxn w/ object
-    const compiledHtml = template(context);
-    // assign
-    document.getElementById('project-list').innerHTML = compiledHtml;
-}
+// HANDLEBARS for projects pg
+projects();
