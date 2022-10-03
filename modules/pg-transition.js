21b053384f3d21cf817726ec052da1f9b69036f9
Anthony C <gitanthony@yahoo.com>
2022-10-03 13:15:39 -0400

    init pg-transition module

---
+++ modules/pg-transition.js
@@ -0,0 +1,36 @@
+const transitionEl = document.querySelector('.transition');
+
+// promisified setTimeout - to always resolve
+const timer = ms => new Promise(resolve => setTimeout(resolve, ms));
+// const timer = function (ms) {
+//     return new Promise(function (resolve) {
+//         setTimeout(resolve, ms);
+//     });
+// };
+
+// will always resolve
+const removeOverlay = async ms => {
+    await timer(ms);
+    transitionEl.classList.remove('is-active');
+};
+
+const navigatingTransition = e => {
+    // prevent going to new page
+    e.preventDefault();
+
+    // allow going to new page
+    let target = e.target.closest('a').href; // traverse
+    // console.log(target);
+
+    if (target.slice(-1) !== '#') {
+        // add overlay when leaving pg
+        transitionEl.classList.add('is-active');
+        // target pg reveal
+        transitionEl.addEventListener(
+            'transitionend',
+            () => (window.location.href = target)
+        );
+    }
+};
+
+export const pgTransitionObj = { removeOverlay, navigatingTransition };
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // for projects pg
 import projects from './modules/projects.js';
 
+import { pgTransitionObj } from './modules/pg-transition.js';
+
 /*
 const bgImg = async () =>
     (document.body.style.background =
@@ -13,38 +15,14 @@ window.addEventListener('load', async () => {
     // corrects for back/forward on desktop browser, but 1) drops page-transition altogether on firefox 2) page "unload" transition dropped on chrome (& at least 2 non-ios, mobile browsers) but page "load" transition maintained.
     // also the nav elements don't always adjust color as they should. Could use dom traversing to deselect the icon color when another icon is hovered/focussed
 
-    const transitionEl = document.querySelector('.transition');
-    const navAnchors = document.querySelectorAll('nav a, .pg-transition');
-    // promisify setTimeout - fxn that returns a Promise
-    const timer = ms => new Promise(resolve => setTimeout(resolve, ms));
-
     // await bgImg();
 
-    // timer(200).then(() => transitionEl.classList.remove('is-active')); // set time as desired
-    let shortDelay = await timer(200);
-    shortDelay = transitionEl.classList.remove('is-active'); // set time as desired
+    pgTransitionObj.removeOverlay(200);
+
+    const navAnchors = document.querySelectorAll('nav a, .pg-transition');
 
     navAnchors.forEach(anchor => {
-        anchor.addEventListener('click', e => {
-            // prevent going to new page
-            e.preventDefault();
-
-            // allow going to new page
-            let target = e.target.closest('a').href; // traverse
-            // console.log(target);
-
-            if (target.slice(-1) !== '#') {
-                // add overlay when leaving pg
-                transitionEl.classList.add('is-active');
-
-                // target pg reveal
-                // timer(400).then(() => (window.location.href = target)); // set time same as transition duration in CSS
-                transitionEl.addEventListener(
-                    'transitionend',
-                    () => (window.location.href = target)
-                );
-            }
-        });
+        anchor.addEventListener('click', pgTransitionObj.navigatingTransition);
     });
 });
 // console.log(window);
@@ -54,7 +32,7 @@ window.addEventListener('load', async () => {
 window.addEventListener('pageshow', event => {
     if (event.persisted) {
         console.log('restored from bfcache');
-        window.location.reload();
+        // window.location.reload();
     } else {
         console.log('loaded manually');
     }
