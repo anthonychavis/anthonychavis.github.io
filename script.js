2830b1a4ff4a7dae46b8061ab55d9c9c34c8c37d
Anthony Chavis <gitanthony@yahoo.com>
2023-09-30 08:42:23 -0400

    persited check removed

--- script.js
+++ script.js
@@ -7,30 +7,10 @@ import { pgTransitionObj } from './modules/pg-transition.js';
 window.addEventListener('load', () => {
     pgTransitionObj.addTransitionDelay(250);
     pgTransitionObj.removeOverlay();
-
     const navAnchors = document.querySelectorAll('nav a, .pg-transition');
-
     navAnchors.forEach(pgTransitionObj.navAnchorClick);
+    return;
 });
-
-// check for persisted
-window.addEventListener('pageshow', event => {
-    // const transitionEl = document.querySelector('.transition');
-    if (event.persisted) {
-        console.log('1) restored from bfcache');
-        window.location.reload(); // cause increased data use? lighter, viable soln w/o framework?
-
-        // transitionEl.classList.remove('is-active'); // when using "back btn" doesn't allow pg to remain --> goes back to former pg
-    } else {
-        console.log('1) loaded manually');
-    }
-});
-
-// window.addEventListener('pagehide', e =>
-//     e.persisted
-//         ? console.log('2) restored from bfcache')
-//         : console.log('2) loaded manually')
-// );
 
 // HANDLEBARS for projects pg
 projects();
